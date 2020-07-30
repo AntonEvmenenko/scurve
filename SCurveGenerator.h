f6faf9718842a434e000c9ebabb3bd5d1ac6a3f0
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2020-07-30 11:25:51 +0200

    refactoring

--- SCurveGenerator.h
+++ SCurveGenerator.h
@@ -5,35 +5,31 @@
 
 constexpr int sCurveLookupTableSize = sizeof(sCurveLookupTable) / sizeof(sCurveLookupTable[0]);
 
-#ifdef PI
-#undef PI
-#endif
+constexpr float pi = 3.14159265358979f;
+constexpr float pi2 = pi * pi;
 
-constexpr float PI = 3.14159265358979f;
-constexpr float PI2 = PI * PI;
-
-constexpr float at = 2 * PI2 / sCurveLookupTableSize;
+constexpr float at = 2 * pi2 / sCurveLookupTableSize;
 
 enum class SCurveGeneratorStage
 {
     STOP,
     ACCELERATION,
-    DECCELERATION,
+    DECELERATION,
     CRUISE
 };
 
 class SCurveGenerator {
 public:
     // transitionTimeUS -- time of acceleration / deceleration stage in microseconds
-    // cruiseDelayUS -- time between stepper motor steps in microseconds
+    // cruiseDelayUS -- time between stepper motor steps in microseconds in cruise mode
     SCurveGenerator(uint32_t transitionTimeUS, uint32_t cruiseDelayUS) {
-        // acceleration_deceleration_time_us = 2 * PI * kt
-        kt = static_cast<float>(transitionTimeUS) / (2 * PI);
+        // transitionTimeUS = 2 * pi * kt
+        kt = static_cast<float>(transitionTimeUS) / (2 * pi);
 
-        // cruise_speed_us = (PI * kt / N);
-        numberOfTransitionSteps = (PI * kt) / cruiseDelayUS;
+        // cruise_speed_us = (pi * kt / numberOfTransitionSteps);
+        numberOfTransitionSteps = (pi * kt) / cruiseDelayUS;
 
-        deltaCruise = (PI * kt / numberOfTransitionSteps);
+        deltaCruise = (pi * kt / numberOfTransitionSteps);
     }
 
     uint32_t getDelayUS()
@@ -42,7 +38,7 @@ public:
             if (i == numberOfTransitionSteps) {
                 stage = SCurveGeneratorStage::CRUISE;
             } else {
-                float d = 2 * PI2 * i / numberOfTransitionSteps;
+                float d = 2 * pi2 * i / numberOfTransitionSteps;
                 t = solve(d) * kt;
                 delta = t - tPrevious;
                 tPrevious = t;
@@ -51,11 +47,11 @@ public:
             }
         } else if (stage == SCurveGeneratorStage::CRUISE) {
             delta = deltaCruise;
-        } else if (stage == SCurveGeneratorStage::DECCELERATION) {
+        } else if (stage == SCurveGeneratorStage::DECELERATION) {
             if (i == 0) {
                 stage = SCurveGeneratorStage::STOP;
             } else {
-                float d = 2 * PI2 * i / numberOfTransitionSteps;
+                float d = 2 * pi2 * i / numberOfTransitionSteps;
                 t = solve(d) * kt;
                 delta = tPrevious - t;
                 tPrevious = t;
@@ -79,7 +75,7 @@ public:
     {
         if (stage == SCurveGeneratorStage::CRUISE) {
             i = numberOfTransitionSteps;
-            stage = SCurveGeneratorStage::DECCELERATION;
+            stage = SCurveGeneratorStage::DECELERATION;
         }
     }
 
@@ -96,7 +92,7 @@ public:
 private:
     float solve(float t) 
     {
-        if (t >= 2 * PI2)
+        if (t >= 2 * pi2)
             return sCurveLookupTable[sCurveLookupTableSize - 1];
         int n = (t / at);
         float dt = t - n * at;
@@ -108,7 +104,7 @@ private:
     float delta = 0;
     float deltaCruise = 0;
     float t = 0, tPrevious = 0;
-    float kt; // time adjust constant, accel time = 2*PI*kt
+    float kt; // time adjust constant, accel time = 2 * pi * kt
 
     SCurveGeneratorStage stage = SCurveGeneratorStage::STOP;
 };
